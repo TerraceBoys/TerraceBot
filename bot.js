86a2e0ce55ae073b94c758e55b5830f4f33f4592
roconnor <roconnor@hubspot.com>
2015-10-15 21:18:55 -0400

    change response

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = cool();
+  botResponse = "this is us";
 
   options = {
     hostname: 'api.groupme.com',
