109d06609f75562aa5c531942051204d42f4ce67
Branden Rodgers <brodgers@nanigans.com>
2015-10-15 23:37:08 -0400

    testing gifs

--- bot.js
+++ bot.js
@@ -6,22 +6,27 @@ var botID = process.env.BOT_ID;
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegex = /^\/cool guy$/;
+      botAnimate = /^animate me$/;
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
-    postMessage();
+    postMessage(cool());
     this.res.end();
-  } else {
+  } else if (request.text && botAnimate.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage('http://38.media.tumblr.com/1c1dd021669887f91c132b8b3606f5f7/tumblr_nw8hxmNKrR1uewx7xo1_400.gif');
+    this.res.end();
+  }
+  else {
     console.log("don't care");
     this.res.writeHead(200);
     this.res.end();
   }
 }
 
-function postMessage() {
-  var botResponse, options, body, botReq;
-
-  botResponse = "this is us";
+function postMessage(message) {
+  var botResponse = message;
+  var options, body, botReq;
 
   options = {
     hostname: 'api.groupme.com',
