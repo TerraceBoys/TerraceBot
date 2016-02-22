06f91cb315bacabcec1e6d1eafc953b83762db31
Brian Cox <brian5021@verizon.net>
2016-02-21 22:22:55 -0500

    Koos

--- bot.js
+++ bot.js
@@ -61,7 +61,8 @@ function respond() {
     this.res.writeHead(200);
     var x = Math.floor((Math.random()*parthArray.length));
     setTimeout(function() {
-      postMessage(parthArray[x]);
+      postMessage("Fuck off")
+      // postMessage(parthArray[x]);
     }, 1500);
     this.res.end();
   } else {
